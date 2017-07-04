b741a4e79aa892b05e788e17dd60f048cefe12c0
andreasbehnke <ab@andreasbehnke.com>
2017-07-04 10:42:09 +0200

    started implementing command line tool

---
+++ src/main/java/org/trie/Main.java
@@ -0,0 +1,13 @@
+package org.trie;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Reading words from dictionary...");
+        Trie.createFromWordList(args[0], Charset.forName("ISO-8859-1"));
+        System.out.println("Finished creating Trie.");
+    }
+}
